8aa53e4f8d5c2f663774fc2d3d36b7ff2acdd52c
Szajkovics Sándor <37326703+KEBABSELLER6@users.noreply.github.com>
2022-08-19 18:43:37 +0200

    moved number of pages to a query

--- scraper.py
+++ scraper.py
@@ -6,12 +6,17 @@ base_url = 'https://www.mavcsoport.hu'
 pagination = ''
 jobs = []
 
-for page in range(0, 49):
+pages_url = f'{base_url}/mav-csoport/allasajanlataink'
+pages_response = requests.get(pages_url)
+doc = BeautifulSoup(pages_response.text, 'html.parser')
+number_of_pages = doc.find(class_='pager-last').a['href'].split('page=')
+
+for page in range(0, number_of_pages[1] + 1):
     if page != 0:
-        pagination = f"?page={page}"
-    url = f"{base_url}/mav-csoport/allasajanlataink{pagination}"
-    result = requests.get(url)
-    doc = BeautifulSoup(result.text, 'html.parser')
+        pagination = f'?page={page}'
+    url = f'{base_url}/mav-csoport/allasajanlataink{pagination}'
+    response = requests.get(url)
+    doc = BeautifulSoup(response.text, 'html.parser')
     parents = doc.find_all(class_='views-row')
     for parent in parents:
         company = parent.find(class_='field-job-company').string.strip()
@@ -28,5 +33,5 @@ print(len(jobs))
 
 json_object = json.dumps(jobs, indent=4)
 
-with open("results.json", "w") as outfile:
+with open('results.json', 'w') as outfile:
     outfile.write(json_object)
